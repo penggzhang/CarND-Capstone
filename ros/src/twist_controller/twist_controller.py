194ca9051a2291927836abb24220c52a1f0ec224
student <jamesjosephreynolds@gmail.com>
2017-09-18 12:18:49 -0700

    remove bad steering and velocity data, write datalog.m for post-processing

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -6,6 +6,7 @@ import rospy
 
 #GAS_DENSITY = 2.858   # TODO: Figure out why we need this - fuel in tank sloshing?
 #ONE_MPH     = 0.44704 # MPH to M/S conversion
+DEBUG_STEER = False
 
 class Controller(object):
 
@@ -27,6 +28,7 @@ class Controller(object):
         self.LPFilt_Thr  = lowpass.LowPassFilter(tau=0.0,ts=self.dt)
         self.LPFilt_Brk  = lowpass.LowPassFilter(tau=0.0,ts=self.dt)
         self.LPFilt_Str  = lowpass.LowPassFilter(tau=0.5,ts=self.dt)
+        self.first = True # first pass flag
         pass
 
     def control(self, proposed_linear_velocity,proposed_angular_velocity,current_linear_velocity,cross_track_error,heading_error,dbw_status):
@@ -40,9 +42,21 @@ class Controller(object):
             throttle = 0.0 
             brake    = 0.0
             steer    = 0.0 # radians -8.2 to 8.2, 14.8 steering gear ratio implies ~31 degrees of play
-            
+
+            # Clip the proposed angular velocity to reasonable values
+            proposed_angular_velocity_clip = proposed_angular_velocity
+            if proposed_angular_velocity_clip > 1.0:
+                proposed_angular_velocity_clip = 1.0
+            if proposed_angular_velocity_clip < -1.0:
+                proposed_angular_velocity_clip = -1.0
+
+            # Clip the proposed linear velocity to reasonable values
+            proposed_linear_velocity_clip = proposed_linear_velocity
+            if proposed_linear_velocity_clip < 0.0:
+                proposed_linear_velocity_clip = -proposed_linear_velocity
+           
             # Compute velocity error
-            vel_err  = proposed_linear_velocity-current_linear_velocity
+            vel_err  = proposed_linear_velocity_clip-current_linear_velocity
             if False:
                 print('vel_err',vel_err)
             if vel_err <= 0.:
@@ -63,14 +77,57 @@ class Controller(object):
             # With yaw controller, this probably needs smaller gains, possibly mostly integral action
             cte_steer = self.PIDCont_Str.step(cross_track_error,self.dt)
 
-           
-            pred_steer = self.YawCont_Str.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
-
+            pred_steer = self.YawCont_Str.get_steering(proposed_linear_velocity_clip, proposed_angular_velocity_clip, current_linear_velocity)
+            
             #print('    PID steer command :',cte_steer)
             #print('Raw Yaw steer command :',pred_steer)
             
-            pred_steer = self.LPFilt_Str.filt(pred_steer) # filter the "feedforward term"
-            steer = pred_steer + cte_steer
+            pred_steer_flt = self.LPFilt_Str.filt(pred_steer) # filter the "feedforward term"
+            steer = pred_steer_flt + cte_steer
+
+            if DEBUG_STEER:
+                # Create a Matlab format log file to determine issue with proposed_angular velocity
+                if self.first:
+                    try:
+                        f = open('datalog.m', 'r+')
+                    except:
+                        f = open('datalog.m', 'w')
+                    self.first = False
+                    f.write('data = [...\n')
+                else:
+                    try:
+                        f = open('datalog.m', 'a+')
+                    except:
+                        f = open('datalog.m', 'w')
+                        self.first = False
+                        f.write('data = [...\n')
+                        
+                f.write(str(proposed_angular_velocity))         # Matlab index 1
+                f.write(', ')
+                f.write(str(proposed_angular_velocity_clip))    # Matlab index 2
+                f.write(', ')
+                f.write(str(proposed_linear_velocity))          # Matlab index 3
+                f.write(', ')
+                f.write(str(proposed_linear_velocity_clip))     # Matlab index 4
+                f.write(', ')
+                f.write(str(brake))                             # Matlab index 5
+                f.write(', ')
+                f.write(str(throttle))                          # Matlab index 6
+                f.write(', ')
+                f.write(str(vel_err))                           # Matlab index 7
+                f.write(', ')
+                f.write(str(cte_steer))                         # Matlab index 8
+                f.write(', ')
+                f.write(str(cross_track_error))                 # Matlab index 9
+                f.write(', ')
+                f.write(str(pred_steer))                        # Matlab index 10
+                f.write(', ')
+                f.write(str(pred_steer_flt))                    # Matlab index 11
+                f.write(', ')
+                f.write(str(steer))                             # Matlab index 12
+                f.write(';...\n')
+                f.close()
+            # end if DEBUG_STEER:
             
             #print('Flt Yaw steer command :',pred_steer)
             #print('  Total steer command :',steer)
